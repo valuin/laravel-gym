660a60295e093fea248afefa277158619d22a036
Valtrizt Khalifah Warri <valvaltrizt@gmail.com>
2024-12-03 18:19:01 +0700

    feat: footer wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,19 +14,17 @@
         />
 
         <!-- Styles / Scripts -->
-        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @else
-        <style>
-        </style>
+        @if (file_exists(public_path('build/manifest.json')) ||
+        file_exists(public_path('hot'))) @vite(['resources/css/app.css',
+        'resources/js/app.js']) @else
+        <style></style>
         @endif
-        
-    </head> 
+    </head>
     <body
-        class="font-sans antialiased text-white h-full bg-cover bg-center p-5"
-        style="background-image: url('{{ asset('bg.png') }}');"
+        class="font-sans antialiased text-white min-h-screen w-full flex flex-col bg-cover bg-center"
+        style="background-image: url('{{ asset('bg.png') }}')"
     >
-        <div class="container mx-auto px-4 py-5 relative z-10">
+        <div class="mx-auto flex-grow p-10 w-full py-10 relative mb-10 z-10">
             <!-- Header -->
             <header class="flex justify-between items-center">
                 <div class="logo flex items-center">
@@ -65,7 +63,7 @@
                 </div>
             </header>
             <!-- Main Content -->
-            <main class="text-start mt-20">
+            <main class="text-start w-full mt-20">
                 <h1 class="text-6xl font-bold mb-5">RamsFit</h1>
                 <h2 class="text-2xl mb-10">THE BEST GYM IN TOWN</h2>
                 <a
@@ -112,16 +110,97 @@
                 </section>
             </main>
             <!-- Footer -->
-            <footer class="mt-20">
-                <h2 class="text-2xl mb-5">Contact Us</h2>
-                <div
-                    class="footer-content flex flex-col md:flex-row justify-between space-y-4 md:space-y-0"
-                >
-                    <p>Address: Jalan Kebugaran</p>
-                    <p>Phone: +62 8129897767</p>
-                    <p>Email: info@ramsfit.com</p>
-                </div>
-            </footer>
         </div>
+        <footer
+            class="grid grid-cols-1 justify-items-center items-center min-h-full space-y-4 bg-stone-900 w-full md:p-8"
+        >
+            <div
+                class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 justify-items-center items-center w-full p-4"
+                id="contacts"
+            >
+                <div
+                    class="flex flex-col text-white items-center md:items-start justify-center"
+                >
+                    <div class="flex gap-4 text-6xl font-bold mb-5">
+                        <img
+                            src="{{ asset('icon.png') }}"
+                            alt="logo"
+                            class="w-16"
+                        />
+                        Ramsfit
+                    </div>
+                    Stay connected—join us on social media and discover stories
+                    waiting to unfold.
+                    <div class="flex flex-row gap-6 mt-6">
+                        <a href="https://twitter.com/elonmusk" target="_blank" rel="noopener noreferrer" aria-label="Visit our Twitter profile">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-8 mt-1"><path fill="#ffffff" d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+                        </a>
+                        <a href="https://www.instagram.com/sisforbrilliance_/" target="_blank" rel="noopener noreferrer" aria-label="Visit our Instagram profile">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-8"><path fill="#ffffff" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+                        </a>
+                        <a href="https://www.facebook.com/Facebook" target="_blank" rel="noopener noreferrer" aria-label="Visit our Facebook profile">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-7 mt-1"><path fill="#ffffff" d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/></svg>
+                        </a>
+                    </div>
+                </div>
+                <div
+                    class="flex flex-col text-white items-start justify-center md:border-r-2 border-white w-full md:w-auto pr-4 md:pr-16 ml-8"
+                >
+                    <p
+                        class="text-center font-semibold text-xl mb-3 md:text-start"
+                    >
+                        Membership
+                    </p>
+                    <div class="space-y-2">
+                        <p class="text-center md:text-start">Membership</p>
+                        <p class="text-center md:text-start">Join Online</p>
+                    </div>
+                </div>
+                <div
+                    class="flex flex-col text-white items-start justify-center md:border-r-2 border-white w-full md:w-auto pr-4 md:pr-24"
+                >
+                    <p
+                        class="text-center font-semibold text-xl mb-3 md:text-start"
+                    >
+                        Classes
+                    </p>
+                    <div class="space-y-2">
+                        <p class="text-center md:text-start">Classes</p>
+                        <p class="text-center md:text-start">Book Online</p>
+                    </div>
+                </div>
+                <div
+                    class="flex flex-col md:border-r-2 border-white text-white items-start justify-center w-full md:w-auto pr-4 md:pr-24"
+                >
+                    <p
+                        class="text-center font-semibold text-xl mb-3 md:text-start"
+                    >
+                        Branch
+                    </p>
+                    <div class="space-y-2">
+                        <p class="text-center md:text-start">Near You</p>
+                        <p class="text-center md:text-start">Our Branches</p>
+                    </div>
+                </div>
+                <div
+                    class="flex flex-col text-white items-start justify-center w-full md:w-auto pr-4 md:pr-24"
+                >
+                    <p
+                        class="text-center font-semibold text-xl mb-3 md:text-start"
+                    >
+                        About
+                    </p>
+                    <div class="space-y-2">
+                        <p class="text-center md:text-start">Near You</p>
+                        <p class="text-center md:text-start">Our Branches</p>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full bottom- flex flex-col justify-center">
+                <p class="text-white text-center">
+                    © 2024 Ramsfit. All rights reserved.
+                </p>
+            </div>
+        </footer>
     </body>
 </html>
