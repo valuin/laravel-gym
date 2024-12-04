3f5534997c75625fb3ee02c50dbb433e3dcc9412
Valtrizt Khalifah Warri <valvaltrizt@gmail.com>
2024-12-04 20:45:11 +0700

    chore: routing for aya 2

---
+++ resources/views/location.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+
+// divnya Replace disini buat aya
+
+<div class="container mx-auto py-8">
+    <h1 class="text-4xl font-bold text-center mb-8">Location</h1>
+</div>
+
+
+@endsection
