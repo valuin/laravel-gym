6a872d88d7437e3037c50fe238d19a65cc65f9f4
Valtrizt Khalifah Warri <valvaltrizt@gmail.com>
2024-12-04 21:23:14 +0700

    chore: routing aul setup

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+
+// replace divnya disini untuk aul
+<div class="container mx-auto py-8">
+    <h1 class="text-4xl font-bold text-center mb-8">About Us</h1>
+</div>
+
+@endsection
