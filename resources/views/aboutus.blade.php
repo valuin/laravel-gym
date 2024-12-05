b5b9fe6281eccb55cd5ad14c9d7e4e1308dd0036
Valtrizt Khalifah Warri <valvaltrizt@gmail.com>
2024-12-05 21:50:16 +0700

    feat: about us values

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,127 +1,120 @@
 @extends('layouts.app') @section('content')
 
-<div class="flex flex-wrap w-full mx-auto overflow-hidden">
-    <!-- Image Left -->
-    <div class="w-full md:w-1/3 overflow-hidden order-2 md:order-1">
-        <img
-            src="{{ asset('about1.png') }}"
-            alt="Ramfit Image"
-            class="w-full h-full object-cover rounded-tr-[10%] rounded-br-[10%]"
-        />
-    </div>
-    <!-- Content -->
-    <div
-        class="w-full md:w-1/3 p-6 flex flex-col justify-center order-1 md:order-2"
-    >
-        <h2 class="text-4xl font-bold mb-6">
-            At RamsFit, We Are Committed...
-        </h2>
-        <p class="text-base leading-relaxed mb-6 text-justify">
-            We are empowering individuals to achieve their fitness goals
-            through a supportive and inclusive environment. With
-            state-of-the-art equipment, personalized training programs, and
-            expert instructors, we strive to cater to all levels of fitness,
-            from beginners to seasoned athletes.
-        </p>
-        <a
-            href="#testimonials"
-            class="inline-block bg-white text-black px-4 py-2 rounded-md font-bold hover:bg-green-400 hover:text-white transition duration-200"
-            >View More</a
-        >
-    </div>
-    <!-- Image Right -->
-    <div class="w-full md:w-1/3 overflow-hidden order-3 md:order-3">
-        <img
-            src="{{ asset('about2.png') }}"
-            alt="Ramfit Image"
-            class="w-full h-full object-cover rounded-tl-[10%] rounded-bl-[10%]"
-        />
+<div class="container mx-auto py-8">
+    <!-- Header Section -->
+    <div class="flex flex-col items-center justify-center px-4">
+        <h1 class="text-6xl font-extrabold text-center mb-8">FIRST IN TOWN</h1>
+        <div class="flex w-full">
+            <!-- Foto sebelah kiri -->
+            <div class="flex-1">
+                <img
+                    src="{{ asset('about1.png') }}"
+                    alt="Gym Image 1"
+                    class="rounded-l-lg h-auto w-full object-cover"
+                />
+            </div>
+            <div
+                class="flex flex-col justify-center items-center text-white p-20 w-1/2"
+            >
+                <p class="text-lg text-justify leading-relaxed">
+                    At RamsFit, we are committed to empowering individuals to
+                    achieve their fitness goals through a supportive and
+                    inclusive environment. With state-of-the-art equipment,
+                    personalized training programs, and expert instructors, we
+                    strive to cater to all levels of fitness, from beginners to
+                    seasoned athletes.
+                </p>
+            </div>
+            <!-- Foto sebelah kanan -->
+            <div class="flex-1">
+                <img
+                    src="{{ asset('about2.png') }}"
+                    alt="Gym Image 2"
+                    class="rounded-r-lg h-auto w-full object-cover"
+                />
+            </div>
+        </div>
     </div>
 </div>
 
-<div class="container mx-auto py-8">
-    <h2 class="text-5xl font-bold text-center mb-10">
-        WHAT THEY SAID ABOUT US
-    </h2>
+<!-- About section -->
+<div
+    class="container mx-auto flex flex-col md:flex-row items-center py-12 px-6 gap-x-16"
+>
+    <div class="text-white md:w-1/3 mb-8 md:mb-0 pr-8">
+        <h2 class="text-4xl font-bold mb-6">Our Code</h2>
+        <p class="text-lg leading-relaxed">
+            Strength drives us, discipline guides us, and excellence defines our
+            fitness journey.
+        </p>
+    </div>
 
-    <div class="flex flex-col space-y-8">
-        <!-- Testimonial 1 -->
+    <div
+        class="text-black grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:w-2/3"
+    >
+        <!-- Box 1 -->
         <div
-            class="bg-white bg-opacity-10 backdrop-blur-sm rounded-lg p-6 shadow-lg flex items-center hover:shadow-xl transition duration-300"
+            class="bg-white/10 backdrop-blur-sm rounded-lg text-white p-6 hover:bg-white/20 transition-all border border-white/20 w-full"
         >
-            <img
-                src="{{ asset('testimony-1.png') }}"
-                alt="Lala Martinez"
-                class="w-32 h-32 rounded-md mr-6"
-            />
-            <div id="testimonials" class="text-white">
-                <p class="text-base leading-relaxed mb-4">
-                    "This gym has everything I need for a great workout. The
-                    equipment is modern and well-maintained, and the staff are
-                    always friendly and helpful. The cleanliness of the facility
-                    is also impressive, making it a comfortable place to
-                    exercise. Highly recommended!"
-                </p>
-                <p class="font-bold">Lala Martinez</p>
-                <p class="italic">Student</p>
-            </div>
+            <h3 class="text-xl font-bold mb-4 text-center">OUR MANIFESTO</h3>
+            <p class="text-center font-medium leading-relaxed">
+                We break limits. Our clubs inspire you to move, sweat, and
+                embrace your fitness journey with boldness and confidence.
+            </p>
         </div>
-        <!-- Testimonial 2 -->
+        <!-- Box 2 -->
         <div
-            class="bg-white bg-opacity-10 backdrop-blur-sm rounded-lg p-6 shadow-lg flex items-center hover:shadow-xl transition duration-300"
+            class="bg-white/10 backdrop-blur-sm rounded-lg text-white p-6 hover:bg-white/20 transition-all border border-white/20 w-full"
         >
-            <img
-                src="{{ asset('testimony-2.png') }}"
-                alt="Stevan Carpenter"
-                class="w-32 h-32 rounded-md mr-6"
-            />
-            <div class="text-white">
-                <p class="text-base leading-relaxed mb-4">
-                    "Fantastic place to work out. The atmosphere is motivating,
-                    and the classes are fun!"
-                </p>
-                <p class="font-bold">Stevan Carpenter</p>
-                <p class="italic">Engineer</p>
-            </div>
+            <h3 class="text-xl font-bold mb-4 text-center">
+                OUR SIGNATURE STYLE
+            </h3>
+            <p class="text-center font-medium leading-relaxed">
+                Expert-designed workouts: Energizing, empowering, transforming.
+                Your journey to greatness begins now!
+            </p>
         </div>
-        <!-- Testimonial 3 -->
+        <!-- Box 3 -->
         <div
-            class="bg-white bg-opacity-10 backdrop-blur-sm rounded-lg p-6 shadow-lg flex items-center hover:shadow-xl transition duration-300"
+            class="bg-white/10 backdrop-blur-sm rounded-lg text-white p-6 hover:bg-white/20 transition-all border border-white/20 w-full"
         >
-            <img
-                src="{{ asset('testimony-1.png') }}"
-                alt="Josef Mehmed"
-                class="w-32 h-32 rounded-md mr-6"
-            />
-            <div class="text-white">
-                <p class="text-base leading-relaxed mb-4">
-                    "The atmosphere at this gym is fantastic. It’s a friendly
-                    and motivating environment, and I feel comfortable working
-                    out here. Whether you're a beginner or experienced, you’ll
-                    find a supportive community that encourages you to meet your
-                    fitness goals."
-                </p>
-                <p class="font-bold">Josef Mehmed</p>
-                <p class="italic">Teacher</p>
-            </div>
+            <h3 class="text-xl font-bold mb-4 text-center">OUR MISSION</h3>
+            <p class="text-center font-medium leading-relaxed">
+                We're all about making fitness fun—empowering you to look and
+                feel your absolute best.
+            </p>
         </div>
-        <!-- Testimonial 4 -->
+        <!-- Box 4 -->
         <div
-            class="bg-white bg-opacity-10 backdrop-blur-sm rounded-lg p-6 shadow-lg flex items-center hover:shadow-xl transition duration-300"
+            class="bg-white/10 backdrop-blur-sm rounded-lg text-white p-6 hover:bg-white/20 transition-all border border-white/20 w-full"
         >
-            <img
-                src="{{ asset('testimony-2.png') }}"
-                alt="Julia Rodrigo"
-                class="w-32 h-32 rounded-md mr-6"
-            />
-            <div class="text-white">
-                <p class="text-base leading-relaxed mb-4">
-                    "Love this gym! Clean, spacious, and the staff is super
-                    friendly."
-                </p>
-                <p class="font-bold">Julia Rodrigo</p>
-                <p class="italic">Pharmacist</p>
-            </div>
+            <h3 class="text-xl font-bold mb-4 text-center">WE'VE GOT EDGE</h3>
+            <p class="text-center font-medium leading-relaxed">
+                We revolutionize fitness, break boundaries, ignite confidence,
+                and inspire bold transformations.
+            </p>
+        </div>
+        <!-- Box 5 -->
+        <div
+            class="bg-white/10 backdrop-blur-sm rounded-lg text-white p-6 hover:bg-white/20 transition-all border border-white/20 w-full"
+        >
+            <h3 class="text-xl font-bold mb-4 text-center">
+                WE'RE ALL ABOUT FUN
+            </h3>
+            <p class="text-center font-medium leading-relaxed">
+                A vibrant, social tribe embracing joy and energy. Life's a
+                celebration, and we bring the party to fitness!
+            </p>
+        </div>
+        <!-- Box 6 -->
+        <div
+            class="bg-white/10 backdrop-blur-sm rounded-lg text-white p-6 hover:bg-white/20 transition-all border border-white/20 w-full"
+        >
+            <h3 class="text-xl font-bold mb-4 text-center">WE'RE AWESOME</h3>
+            <p class="text-center font-medium leading-relaxed">
+                Life's a grand stage, and we're here to shine like the stars we
+                are!
+            </p>
         </div>
     </div>
 </div>
