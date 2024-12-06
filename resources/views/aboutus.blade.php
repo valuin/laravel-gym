9b00edcabc24bab9437bffa254dc657ae46a140d
CintaAuliya <cintahk2004@gmail.com>
2024-12-06 22:30:05 +0700

    feat: values and mission 2

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -26,6 +26,48 @@
     </div>
 </div>
 
+<!--Team Photo Secion -->
+<div class="container mx-auto py-16 px-4 flex flex-wrap items-center justify-between">
+  <!-- Bagian Kiri: Kotak-kotak -->
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full md:w-2/3">
+    <!-- Kotak 1 -->
+    <div class="text-black bg-gradient-to-b from-[#737373] via-[#A6A6A6] to-[#D9D9D9] rounded-lg p-4 flex flex-col items-center">
+      <img src="aboutimg1/ariq2.jpg" alt="Ariq Adlan" class="w-full h-full rounded-lg mb-4">
+      <h3 class="text-lg font-bold text-center">Ariq Adlan</h3>
+      <p class="text-sm text-center">Pilates Instructor</p>
+    </div>
+    <!-- Kotak 2 -->
+    <div class="text-black bg-gradient-to-b from-[#737373] via-[#A6A6A6] to-[#D9D9D9] rounded-lg p-4 flex flex-col items-center">
+      <img src="aboutimg1/ciul.jpg" alt="Cinta Auliya" class="w-full h-full rounded-lg mb-4">
+      <h3 class="text-lg font-bold text-center">Cinta Auliya</h3>
+      <p class="text-sm text-center">Yoga Instructor</p>
+    </div>
+    <!-- Kotak 3 -->
+    <div class="text-black bg-gradient-to-b from-[#737373] via-[#A6A6A6] to-[#D9D9D9] rounded-lg p-4 flex flex-col items-center">
+      <img src="aboutimg1/rama.jpg" alt="Ilham Rizky" class="w-full h-full rounded-lg mb-4">
+      <h3 class="text-lg font-bold text-center">Ilham Rizky</h3>
+      <p class="text-sm text-center">Pilates Instructor</p>
+    </div>
+    <!-- Kotak 4 -->
+    <div class="text-black bg-gradient-to-b from-[#737373] via-[#A6A6A6] to-[#D9D9D9] rounded-lg p-4 flex flex-col items-center">
+      <img src="aboutimg1/aya.jpg" alt="Layalia Nur" class="w-full h-full rounded-lg mb-4">
+      <h3 class="text-lg font-bold text-center">Layalia Nur</h3>
+      <p class="text-sm text-center">Yoga Instructor</p>
+    </div>
+    <!-- Kotak 5 -->
+    <div class="text-black bg-gradient-to-b from-[#737373] via-[#A6A6A6] to-[#D9D9D9] rounded-lg p-4 flex flex-col items-center">
+      <img src="aboutimg1/vall.png" alt="Valtrizt" class="bg-white w-full h-full rounded-lg mb-4">
+      <h3 class="text-lg font-bold text-center">Valtrizt</h3>
+      <p class="text-sm text-center">Body Combat Instructor</p>
+    </div>
+  </div>
+  <!-- Bagian Kanan -->
+  <div class="flex-1 flex flex-col justify-center text-center md:text-right mt-8 md:mt-0">
+    <h2 class="text-4xl font-bold">Meet Our Team</h2>
+    <p class="text-lg mt-4">Let's make your journey extraordinary!</p>
+  </div>
+</div>
+
 <!-- About section -->
 <div class="container mx-auto flex flex-col md:flex-row items-center py-12 px-6 gap-x-16">
     <div class="text-white md:w-1/3 mb-8 md:mb-0 pr-8">
