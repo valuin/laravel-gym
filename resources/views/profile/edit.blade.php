a9e599d58c9e6b54def038010624decf9c6d1734
Valtrizt Khalifah Warri <valvaltrizt@gmail.com>
2024-12-08 22:26:57 +0700

    style: edit profile modals

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -10,19 +10,19 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white bg-white/10 backdrop-blur-sm rounded-lg border-2 border-white/15  hover:border-emerald-500/30 hover:shadow-emerald-500/30 transition-all shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white bg-white/10 backdrop-blur-sm rounded-lg border-2 border-white/15  hover:border-emerald-500/30 hover:shadow-emerald-500/30 transition-all shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white bg-white/10 backdrop-blur-sm rounded-lg border-2 border-white/15  hover:border-emerald-500/30 hover:shadow-emerald-500/30 transition-all shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
